e3c38249b8038ee8b9185f8923d3b23da29bfa62
asterix135 <asterix135@users.noreply.github.com>
2015-11-06 17:16:56 -0500

    Update euler009.py

--- project_euler/euler009.py
+++ project_euler/euler009.py
@@ -2,3 +2,10 @@
 https://www.hackerrank.com/contests/projecteuler/challenges/euler009
 """
 
+# form a pythagorean triple summing to N
+# N = 2n**2 + 2 mn
+# a = n**2 - m**2
+# b = 2nm
+# c = n**2 + m**2
+
+# smallest is 3, 4, 5; N=12, n=1, m=2
